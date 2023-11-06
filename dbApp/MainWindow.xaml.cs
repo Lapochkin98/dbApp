7ae0442f9faab3042b8f7055f382137cffbfdea0
Lapochkin98 <90198600+Lapochkin98@users.noreply.github.com>
2023-11-06 21:20:39 +0300

    06.11.2023

--- dbApp/MainWindow.xaml.cs
+++ dbApp/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace dbApp
 {
@@ -30,6 +17,7 @@ namespace dbApp
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void ExecuteQuery2(object sender, RoutedEventArgs e)
